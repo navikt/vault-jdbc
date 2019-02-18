c66df4980968451fbef30c9fdaf0129c36f5d560
Eirik <eiriksletteberg@gmail.com>
2019-02-18 15:03:37 +0100

    Add secrets and deploy code

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_45bbcaff3fce_key -iv $encrypted_45bbcaff3fce_iv \
+      -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+
+    gpg --fast-import travis/codesigning.asc
+    mvn --settings travis/settings.xml deploy -Prelease -DskipTests=true
+fi
